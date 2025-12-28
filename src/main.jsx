4515c35f192bbca151e465c749f60290c34f5c89
playf0100 <playf0100@gmail.com>
2025-12-28 07:12:00 -0800

    www

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,8 @@ import App from './App.jsx'
 import { I18nProvider } from './i18n.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <HashRouter basename="/twevil">
+  // CHANGE THIS LINE: Remove basename="/twevil"
+  <HashRouter>
     <I18nProvider defaultLang="zh-Hans">
       <App />
     </I18nProvider>
